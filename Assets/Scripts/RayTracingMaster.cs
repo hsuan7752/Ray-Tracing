275a3ce7c03336e610b7296e0e02d3cd93ffe9fe
hsuan7752 <hsuan7752@gmail.com>
2022-02-25 11:14:52 +0800

    Modified Tracing(Ground Plane & Sphere)

--- Assets/Scripts/RayTracingMaster.cs
+++ Assets/Scripts/RayTracingMaster.cs
@@ -1,24 +1,48 @@
 ﻿using UnityEngine;
+
 public class RayTracingMaster : MonoBehaviour
 {
     public ComputeShader RayTracingShader;
+
     private RenderTexture _target;
+
+    private Camera _camera;
+
+    public Texture SkyboxTexture;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        SetShaderParameters();
         Render(destination);
     }
+    
     private void Render(RenderTexture destination)
     {
         // Make sure we have a current render target
         InitRenderTexture();
+        
         // Set the target and dispatch the compute shader
         RayTracingShader.SetTexture(0, "Result", _target);
         int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
         int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
         RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        
         // Blit the result texture to the screen
         Graphics.Blit(_target, destination);
     }
+
+    private void SetShaderParameters()
+    {
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+    }
+
     private void InitRenderTexture()
     {
         if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
