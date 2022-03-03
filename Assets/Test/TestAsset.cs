8a8d94a2249a649b475d111640ed7eec9bb3a3b8
CCCpeggy <peggy19925@gmail.com>
2022-03-03 09:37:51 +0800

    Diffuse

---
+++ Assets/Test/TestAsset.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// the output color tutorial asset.
+/// </summary>
+[CreateAssetMenu(fileName = "TestAsset", menuName = "Rendering/TestAsset")]
+public class TestAsset : RayTracingTutorialAsset
+{
+  /// <summary>
+  /// create tutorial.
+  /// </summary>
+  /// <returns>the tutorial.</returns>
+  public override RayTracingTutorial CreateTutorial()
+  {
+    return new Test(this);
+  }
+}
