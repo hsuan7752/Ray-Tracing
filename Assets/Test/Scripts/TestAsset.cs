e43182c73ba738cae536ae41c68ad8a201e2c507
CCCpeggy <peggy19925@gmail.com>
2022-03-07 12:06:38 +0800

    場景建置

--- Assets/Test/Scripts/TestAsset.cs
+++ Assets/Test/Scripts/TestAsset.cs
@@ -15,9 +15,9 @@ public class TestAsset : RayTracingTutorialAsset
   public override RayTracingTutorial CreateTutorial()
   {
     LightSamplePos.Clear();
-    for (float x=-0.5f; x<=0.5f; x+=0.01f) {
-      for (float z=-0.5f; z<=0.5f; z+=0.01f) {
-        LightSamplePos.Add(new Vector3(x, 1.845f, z));
+    for (float x=-5f; x<=20f; x+=1.3f) {
+      for (float z=-2.5f; z<=2.5f; z+=0.8f) {
+        LightSamplePos.Add(new Vector3(x, 1.8f, z));
       }
     }
 
